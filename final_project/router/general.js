90cbc2893384bd09c4595afaf1b0fea904ede60b
Dimah <dimahmehdawi@gmail.com>
2024-02-25 18:17:05 +0300

    add async/await

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -22,33 +22,57 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify(books,null,4));
+public_users.get('/',async function (req, res) {
+  try{
+    const booksList = await JSON.stringify(books,null,4);
+    res.send(booksList);
+  }
+  catch(err){
+    res.send(err)
+  }
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  return res.send(books[req.params.isbn])
+public_users.get('/isbn/:isbn',async function (req, res) {
+  try{
+    const book = await JSON.stringify(books[req.params.isbn],null,4);
+    res.send(book);
+  }
+  catch(err){
+    res.send(err)
+  }
  });
 
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  const author = req.params.author;
-  const authorBooks = [];
-  for(isbn in books){
-    if(books[isbn]["author"]===author)authorBooks.push(books[isbn])
+public_users.get('/author/:author',async function (req, res) {
+  try{
+    const author = req.params.author;
+    const authorBooks = [],currentBooks = await books;
+    for(isbn in currentBooks){
+      const currentBook =  currentBooks[isbn];
+      if(currentBook["author"]===author)authorBooks.push(currentBook)
+    };
+    res.send(authorBooks);
   }
-  return res.send(authorBooks)
+  catch(err){
+    res.send(err)
+  }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  const title = req.params.title;
-  const titleBooks = [];
-  for(isbn in books){
-    if(books[isbn]["title"]===title)titleBooks.push(books[isbn])
+public_users.get('/title/:title',async function (req, res) {
+  try{
+    const title = req.params.title;
+    const titleBooks = [],currentBooks = await books;
+    for(isbn in currentBooks){
+      const currentBook =  currentBooks[isbn];
+      if(currentBook["title"]===title)titleBooks.push(currentBook)
+    };
+    res.send(titleBooks);
   }
-  return res.send(titleBooks)
+  catch(err){
+    res.send(err)
+  }
 });
 
 //  Get book review
